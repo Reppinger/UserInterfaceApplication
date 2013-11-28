9dc40deefe3877507b8a386b9221cb90e6c95170
Randy Eppinger <RandyEppinger@neo.rr.com>
2013-11-28 10:21:02 -0500

    Added textfield and slider controls.

--- app/tab1_controller.rb
+++ app/tab1_controller.rb
@@ -14,18 +14,41 @@ class Tab1Controller < UIViewController
   end
 
   def add_form_elements
+    label = create_label
+    view.addSubview label
+    textfield = create_textfield
+    view.addSubview textfield
+    switch = create_switch
+    view.addSubview switch
+  end
+
+  def create_label
     label = UILabel.alloc.init
     if self.respondsToSelector(:edgesForExtendedLayout)
       self.edgesForExtendedLayout = UIRectEdgeNone
-      label.frame = [[0,0], [300,30]]
+      label.frame = [[0, 0], [300, 30]]
     else
-      label.frame = [[0,40], [300,30]]
+      label.frame = [[0, 40], [300, 30]]
     end
     label.backgroundColor = UIColor.clearColor
     label.textColor = UIColor.greenColor
     label.font = UIFont.fontWithName('HelveticaNeue-CondensedBold', size: 18)
     label.text = 'This is sample application'
-    view.addSubview label
+    label
+  end
+
+  def create_textfield
+    textbox = UITextField.alloc.initWithFrame([[10, 80], [200, 35]])
+    textbox.borderStyle = UITextBorderStyleRoundedRect
+    textbox.placeholder = 'Type..'
+    textbox.textAlignment = UITextAlignmentCenter
+    textbox
+  end
+
+  def create_switch
+    @switch = UISwitch.alloc.initWithFrame([[10, 130], [200, 35]])
+    @switch.addTarget(self, action: 'switch_is_changed', forControlEvents: UIControlEventValueChanged)
+    @switch
   end
 
   def setupNavigationBar
@@ -39,5 +62,12 @@ class Tab1Controller < UIViewController
     @alert_box.show
   end
 
+  def switch_is_changed
+    if @switch.on?
+
+    else
+
+    end
+  end
 
 end
